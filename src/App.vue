7c5721e16a4513497b09cc02a00134d65be55c0f
Sabin Subedi <69413078+Sabin-Subedi@users.noreply.github.com>
2022-01-20 11:53:17 +0545

    Completed some basics of vue

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>hello</h1>
+  <Modal />
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+};
+</script>
---
+++ src/components/Modal.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>modal content</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+};
+</script>
+
+<style scoped>
+.modal {
+  width: 400;
+  padding: 20px;
+  margin: 100px auto;
+  background: #fff;
+  border-radius: 5px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.527);
+  width: 100%;
+  height: 100%;
+}
+</style>
