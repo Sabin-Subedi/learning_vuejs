f4c1d14842346e5c69eaa5573cc9da5efd54e8fc
Sabin Subedi <69413078+Sabin-Subedi@users.noreply.github.com>
2022-01-21 16:41:44 +0545

    Completed learning fetch and router

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt iste
+      tempora odio in obcaecati beatae ut fuga quo, labore commodi adipisci
+      doloribus autem quasi, illum, harum perspiciatis itaque. Animi enim non
+      voluptatem inventore error consectetur, odio omnis iure repudiandae quia
+      libero. Corporis sit pariatur optio quae nobis! Dicta ad veritatis
+      inventore ducimus fuga vel tempore, minus facere! Labore distinctio
+      pariatur sit, dolor itaque tempore recusandae odit quam veniam impedit
+      cupiditate dolorem aut ad quasi perferendis exercitationem natus
+      blanditiis eius soluta quisquam. Odio voluptatum necessitatibus, minima ex
+      doloremque ipsa animi illum modi tempora mollitia id odit, fuga
+      reiciendis, facilis commodi! Porro?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt iste
+      tempora odio in obcaecati beatae ut fuga quo, labore commodi adipisci
+      doloribus autem quasi, illum, harum perspiciatis itaque. Animi enim non
+      voluptatem inventore error consectetur, odio omnis iure repudiandae quia
+      libero. Corporis sit pariatur optio quae nobis! Dicta ad veritatis
+      inventore ducimus fuga vel tempore, minus facere! Labore distinctio
+      pariatur sit, dolor itaque tempore recusandae odit quam veniam impedit
+      cupiditate dolorem aut ad quasi perferendis exercitationem natus
+      blanditiis eius soluta quisquam. Odio voluptatum necessitatibus, minima ex
+      doloremque ipsa animi illum modi tempora mollitia id odit, fuga
+      reiciendis, facilis commodi! Porro?
+    </p>
+  </div>
+</template>
