e089b214b113cb4a41a509345737ed7cca81ff04
Sabin Subedi <69413078+Sabin-Subedi@users.noreply.github.com>
2022-01-24 10:10:48 +0545

    Learn about the composition api

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">{{ age }}</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  setup() {
+    let name = "mario";
+    let age = 30;
+
+    return {
+      name,
+      age,
+    };
+  },
+};
+</script>
